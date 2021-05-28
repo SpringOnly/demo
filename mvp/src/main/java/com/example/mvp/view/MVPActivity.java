1333388a9d997c7982d5f7a8e4c4af5dcde140b6
Administrator <939006659@qq.com>
2021-05-28 09:30:52 +0800

    update

--- mvp/src/main/java/com/example/mvp/view/MVPActivity.java
+++ mvp/src/main/java/com/example/mvp/view/MVPActivity.java
@@ -3,39 +3,13 @@ package com.example.mvp.view;
 import android.view.View;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import com.alibaba.android.arouter.facade.annotation.Autowired;
 import com.alibaba.android.arouter.facade.annotation.Route;
-import com.chad.library.adapter.base.BaseQuickAdapter;
-import com.chad.library.adapter.base.listener.OnItemClickListener;
 import com.example.commonlibrary.base.BaseActivity;
 import com.example.commonlibrary.base.arouter.ARouterConstant;
-import com.example.commonlibrary.util.LogUtil;
-import com.example.mvc.R;
 import com.example.mvc.databinding.ActivityMvpBinding;
 import com.example.mvp.presenter.MVPPresenter;
 import com.example.mvp.presenter.contract.MVPBannerContract;
-import com.example.mvp.test.TestAdapter;
-import com.example.mvp.test.TestBean;
-import com.trello.rxlifecycle3.android.ActivityEvent;
-
-import org.jetbrains.annotations.NotNull;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
-
-import io.reactivex.Observable;
-import io.reactivex.Observer;
-import io.reactivex.disposables.Disposable;
-import io.reactivex.functions.Action;
-import io.reactivex.functions.Consumer;
-import io.reactivex.functions.Function;
-import io.reactivex.functions.Predicate;
-import io.reactivex.schedulers.Schedulers;
 
 @Route(path = ARouterConstant.MVP)
 public class MVPActivity extends BaseActivity<ActivityMvpBinding> implements MVPBannerContract.MVPView {
@@ -60,34 +34,7 @@ public class MVPActivity extends BaseActivity<ActivityMvpBinding> implements MVP
 
     @Override
     protected void initListener() {
-        GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 3);
-        RecyclerView testRv = Binding.testRv;
-        testRv.setLayoutManager(gridLayoutManager);
 
-        addTestBean("我不知道");
-        addTestBean("哈哈");
-        addTestBean("你在想什么");
-        addTestBean("因为什么");
-        addTestBean("梦醒了吗");
-        addTestBean("多行不义");
-
-        TestAdapter testAdapter = new TestAdapter(R.layout.item_select, testBeanList);
-        testRv.setAdapter(testAdapter);
-        testAdapter.setOnItemClickListener(new OnItemClickListener() {
-            @Override
-            public void onItemClick(@NonNull @NotNull BaseQuickAdapter<?, ?> adapter, @NonNull @NotNull View view, int position) {
-                TestAdapter.temp = position;
-                testAdapter.notifyDataSetChanged();
-            }
-        });
-    }
-
-    List<TestBean> testBeanList = new ArrayList<>();
-
-    public void addTestBean(String type) {
-        TestBean testBean = new TestBean();
-        testBean.setNameType(type);
-        testBeanList.add(testBean);
     }
 
     @Override
